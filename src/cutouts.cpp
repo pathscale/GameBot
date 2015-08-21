01a21f12d325a0562c70cf28179ab908e2d2177d
D.C <pasc.pcz@porcupinefactory.org>
2015-08-21 21:59:15 +0200

    Changed shorthand names

--- src/cutouts.cpp
+++ src/cutouts.cpp
@@ -2,21 +2,22 @@
 #include <QString>
 #include <QPoint>
 
-#define _T(name, path, count, anchor, tiles, threshold) \
+// F for "feature"
+#define _F(name, path, count, anchor, tiles, threshold) \
     std::pair<const QString, const FeatureDesc> \
         (name, \
          FeatureDesc(QString(":/cutouts/") + path, count, threshold, anchor, tiles))
 
-typedef QPoint _P;
+typedef QPoint pos;
 
 // VERY IMPORTANT: all images must be cut out from the same scale; currently 1920x1200 default zoom
 // ALSO IMPORTANT: UI may not scale proportionally to map features, keep in separate FeatureList
 
 const FeatureDescList cutouts {
-//    _T("TH9", "TH9.png", 1, 0.9), // different scale
-    _T("TH10", "TH10.png", 1, _P(25, 12), 4, 0.9),
-    _T("AD8", "defense/ad8.png", 0, _P(17, -8), 3, 0.8),
-    _T("TR0", "props/TR0.png", 0, _P(20, -15), 2, 0.95),
-    _T("SH0", "props/sh0.png", 0, _P(20, -3), 2, 0.95),
+//    _F("TH9", "TH9.png", 1, 0.9), // different scale
+    _F("TH10", "TH10.png", 1, pos(25, 12), 4, 0.9),
+    _F("AD8", "defense/ad8.png", 0, pos(17, -8), -3, 0.8),
+    _F("TR0", "props/TR0.png", 0, pos(20, -15), 2, 0.95),
+    _F("SH0", "props/sh0.png", 0, pos(20, -3), 2, 0.95),
 };
 
