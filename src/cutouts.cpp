6b2c42d40b9aafc4c34d1937f17fb655e2940260
D.C <pasc.pcz@porcupinefactory.org>
2015-08-29 16:14:51 +0200

    Added gold mine

--- src/cutouts.cpp
+++ src/cutouts.cpp
@@ -59,6 +59,7 @@ const FeatureDescList cutouts {
 
     _F("EC12",  "ElixirColl12",  _SINGLE("storage/ec12.png",     pos(0, 0),      0.95),  0,  3, Building(0)),
     _F("DD6",   "DarkDrill6",    _SINGLE("storage/dd6.png",      pos(0, 0),      0.97),  0,  3, Building(0)),
+    _F("GM12",  "GoldMine12",    _SINGLE("storage/gm12.png",     pos(0, 0),      0.95),  0,  3, Building(0)),
 
     _F("GS11-0","GoldStor11_0",  _SINGLE("storage/gold11-0.png", pos(0, 0),      0.95),  0,  3, Storage(0, Storage::GOLD, 0)), // FIXME: health, offset, amount
     _F("DS6-0", "DarkStor6_0",   _SINGLE("storage/ds6-0.png",    pos(0, 0),      0.95),  0,  3, Storage(0, Storage::DARK, 0)), // FIXME: health, offset, amount
