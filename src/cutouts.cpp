faa0d729f443a45933acf089164368510ee3bbe0
D.C <pasc.pcz@porcupinefactory.org>
2015-09-09 17:41:44 +0200

    Adjusted building positions

--- src/cutouts.cpp
+++ src/cutouts.cpp
@@ -36,40 +36,40 @@ typedef QPoint pos;
 
 
 const FeatureDescList cutouts {
-    _F("TH10",  "TownHall10",    _SINGLE("TH10.png",             pos(25, 12),    0.9),   1, new Building(4, 5500)),
+    _F("TH10",  "TownHall10",    _SINGLE("TH10.png",             pos(25, 12),   0.9),   1, new Building(4, 5500)),
     //_F("AD8",   "AirDefense8",  _SINGLE("defense/ad8.png",      pos(17, -8),    0.8),   0,  3, Defense(1170, 10, 320, Defense::SINGLE, Defense::AIR)),
-    _F("CC6",   "ClanCastle6",   _SINGLE("cc6.png",              pos(0, 0),      0.9),   1, new Building(3, 0)),
+    _F("CC6",   "ClanCastle6",   _SINGLE("cc6.png",              pos(20, -18),  0.9),   1, new Building(3, 0)),
 
-    _F("TR0",   "Tree",          _SINGLE("props/TR0.png",        pos(20, -15),   0.95),  0, new Scenery(2)),
-    _F("SH0",   "Shed",          _SINGLE("props/sh0.png",        pos(20, -3),    0.95),  0, new Scenery(2)),
+    _F("TR0",   "Tree",          _SINGLE("props/TR0.png",        pos(20, -15),  0.95),  0, new Scenery(2)),
+    _F("SH0",   "Shed",          _SINGLE("props/sh0.png",        pos(20, -3),   0.95),  0, new Scenery(2)),
 
-    _F("AC8",   "ArmyCamp8",     _SINGLE("army/ac8.png",         pos(0, 0),      0.95),  0, new Building(5, 0)),
-    _F("DF3",   "DarkFactory3",  _SINGLE("army/df3.png",         pos(0, 0),      0.95),  0, new Building(3, 0)),
-    _F("LA8",   "Laboratory8",   _SINGLE("army/la8.png",         pos(0, 0),      0.95),  0, new Building(4, 0)),
-    _F("BA10",  "Barracks10",    _SINGLE("army/ba10.png",        pos(0, 0),      0.95),  0, new Building(3, 0)),
-    _F("DB6",   "DarkBarracks6", _SINGLE("army/db6.png",         pos(0, 0),      0.95),  0, new Building(3, 0)),
+    _F("AC8",   "ArmyCamp8",     _SINGLE("army/ac8.png",         pos(18, -90),  0.95),  0, new Building(5, 0)),
+    _F("DF3",   "DarkFactory3",  _SINGLE("army/df3.png",         pos(8, -46),   0.95),  0, new Building(3, 0)),
+    _F("LA8",   "Laboratory8",   _SINGLE("army/la8.png",         pos(48, -23),  0.95),  0, new Building(4, 0)),
+    _F("BA10",  "Barracks10",    _SINGLE("army/ba10.png",        pos(32, 2),    0.95),  0, new Building(3, 0)),
+    _F("DB6",   "DarkBarracks6", _SINGLE("army/db6.png",         pos(23, 22),   0.95),  0, new Building(3, 0)),
 
-    _F("BK",    "BKingLair",     _SINGLE("army/bk.png",          pos(0, 0),      0.95),  1, new Building(3, 0)), // Special: has range
-    _F("AQ",    "ArcherQueen",   _SINGLE("army/aq.png",          pos(0, 0),      0.95),  1, new Building(3, 0)), // Special: has range
+    _F("BK",    "BKingLair",     _SINGLE("army/bk.png",          pos(17, -11),  0.95),  1, new Building(3, 0)), // Special: has range
+    _F("AQ",    "ArcherQueen",   _SINGLE("army/aq.png",          pos(21, -18),  0.95),  1, new Building(3, 0)), // Special: has range
 
-    _F("AD8",   "AirDefense8",   _SINGLE("defense/ad8_m.png",    pos(0, 0),      0.8),   0, new Defense(3, 1170, 10, 320, Defense::SINGLE, Defense::AIR)), // alpha mask
-    _F("AS4",   "AirSweeper4",   _SINGLE("defense/as4.png",      pos(0, 0),      0.92),  0, new Defense(2, 0, 15, 0, Defense::PUSH, Defense::AIR)), // It can be rotated 45 degrees at a time, and hence has 8 rigid directional settings. The Air Sweeper attacks in a rectangular area of effect of width 5 tiles and length 14. (http://clashofclans.wikia.com/wiki/Air_Sweeper)
+    _F("AD8",   "AirDefense8",   _SINGLE("defense/ad8_m.png",    pos(30, 27),   0.8),   0, new Defense(3, 1170, 10, 320, Defense::SINGLE, Defense::AIR)), // alpha mask
+    _F("AS4",   "AirSweeper4",   _SINGLE("defense/as4.png",      pos(20, -14),  0.92),  0, new Defense(2, 0, 15, 0, Defense::PUSH, Defense::AIR)), // It can be rotated 45 degrees at a time, and hence has 8 rigid directional settings. The Air Sweeper attacks in a rectangular area of effect of width 5 tiles and length 14. (http://clashofclans.wikia.com/wiki/Air_Sweeper)
             // TODO: decrease threshold to ~0.9 after UI is masked out to gain obscured detection
-    _F("CA13",  "Cannon13",      _SINGLE("defense/ca13_2.png",   pos(0, 0),      0.92), 0,  new Defense(3, 0, 9, 0, Defense::SINGLE, Defense::GROUND)),
-    _F("WT8",   "Wizard8",       _SINGLE("defense/wt8.png",      pos(0, 0),      0.95),  0, new Defense(3, 0, 7, 0, Defense::SPLASH1TILE, Defense::AIR | Defense::GROUND)),
-    _F("AT13",  "Archer13",      _SINGLE("defense/at13.png",     pos(0, 0),      0.95),  0, new Defense(3, 0, 10, 0, Defense::SINGLE, Defense::AIR | Defense::GROUND)),
-    _F("MO8",   "Mortar8",       _SINGLE("defense/mo8.png",      pos(0, 0),      0.95),  0, new Defense(3, 0, 11, 0, Defense::SPLASH15TILE, Defense::GROUND)), // FIXME: range is <4, 11>
-    _F("XB4" ,  "XBow4",         _SINGLE("defense/xb4_2.png",    pos(0, 0),      0.95), 0,  new Defense(3, 0, 11, 0, Defense::SINGLE, Defense::AIR | Defense::GROUND)), // FIXME: also Range=14, AIR, depending on setting
+    _F("CA13",  "Cannon13",      _SINGLE("defense/ca13_2.png",   pos(38, -32),  0.92), 0,  new Defense(3, 0, 9, 0, Defense::SINGLE, Defense::GROUND)),
+    _F("WT8",   "Wizard8",       _SINGLE("defense/wt8.png",      pos(23, -10),  0.95),  0, new Defense(3, 0, 7, 0, Defense::SPLASH1TILE, Defense::AIR | Defense::GROUND)),
+    _F("AT13",  "Archer13",      _SINGLE("defense/at13.png",     pos(21, -2),   0.95),  0, new Defense(3, 0, 10, 0, Defense::SINGLE, Defense::AIR | Defense::GROUND)),
+    _F("MO8",   "Mortar8",       _SINGLE("defense/mo8.png",      pos(5, -35),   0.95),  0, new Defense(3, 0, 11, 0, Defense::SPLASH15TILE, Defense::GROUND)), // FIXME: range is <4, 11>
+    _F("XB4" ,  "XBow4",         _SINGLE("defense/xb4_2.png",    pos(39, -32),  0.95), 0,  new Defense(3, 0, 11, 0, Defense::SINGLE, Defense::AIR | Defense::GROUND)), // FIXME: also Range=14, AIR, depending on setting
             // TODO: decrease threshold to ~0.9 after UI masked out
-    _F("IT3",   "Inferno3",      _SINGLE("defense/it3.png",      pos(0, 0),      0.95),  0, new Defense(3, 0, 9, 0, Defense::SINGLE, Defense::AIR | Defense::GROUND)),
+    _F("IT3",   "Inferno3",      _SINGLE("defense/it3.png",      pos(20, 16),   0.95),  0, new Defense(2, 0, 9, 0, Defense::SINGLE, Defense::AIR | Defense::GROUND)),
 
-    _F("EC12",  "ElixirColl12",  _SINGLE("storage/ec12.png",     pos(0, 0),      0.95),  0, new Building(3, 0)),
-    _F("DD6",   "DarkDrill6",    _S({_I("storage/dd6.png",      pos(0, 0),      0.97),
-                                     _I("storage/dd6_1.png",    pos(0, 0),      0.95)}), 0, new Building(3, 0)),
-    _F("GM12",  "GoldMine12",    _SINGLE("storage/gm12.png",     pos(0, 0),      0.95),  0, new Building(3, 0)),
+    _F("EC12",  "ElixirColl12",  _SINGLE("storage/ec12.png",     pos(5, 14),    0.95),  0, new Building(3, 0)),
+    _F("DD6",   "DarkDrill6",    _S({_I("storage/dd6.png",      pos(-9, -14),   0.97),
+                                     _I("storage/dd6_1.png",    pos(-8, -17),   0.95)}), 0, new Building(3, 0)),
+    _F("GM12",  "GoldMine12",    _SINGLE("storage/gm12.png",    pos(30, -19),   0.95),  0, new Building(3, 0)),
 
-    _F("GS11-0","GoldStor11",   _SINGLE("storage/gold11base.png",pos(0, 0),      0.95),  0, new Storage(3, 0, Storage::GOLD, 0)), // FIXME: health, offset, amount
-    _F("DS6-0", "DarkStor6_0",   _SINGLE("storage/ds6-0.png",    pos(0, 0),      0.95),  0, new Storage(3, 0, Storage::DARK, 0)), // FIXME: health, offset, amount
-    _F("ES11-0","ElixirStor11_0",_SINGLE("storage/es11-0.png",   pos(0, 0),      0.95),  0, new Storage(3, 0, Storage::ELIXIR, 0)), // FIXME: health, offset, amount
+    _F("GS11-0","GoldStor11",   _SINGLE("storage/gold11base.png",pos(12, 12),   0.95),  0, new Storage(3, 0, Storage::GOLD, 0)), // FIXME: health, offset, amount
+    _F("DS6-0", "DarkStor6_0",   _SINGLE("storage/ds6-0.png",    pos(16, -9),   0.95),  0, new Storage(3, 0, Storage::DARK, 0)), // FIXME: health, offset, amount
+    _F("ES11-0","ElixirStor11_0",_SINGLE("storage/es11-0.png",   pos(0, 0),     0.95),  0, new Storage(3, 0, Storage::ELIXIR, 0)), // FIXME: health, offset, amount
 };
 
