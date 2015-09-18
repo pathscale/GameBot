b3c0e97187411d1364dda6d48d5ce80fda3a412b
D.C <pasc.pcz@porcupinefactory.org>
2015-09-18 15:43:49 +0200

    cookie: implemented touch end event

--- CookieRun/reader.cpp
+++ CookieRun/reader.cpp
@@ -62,14 +62,18 @@ std::chrono::duration<int, std::milli> float_to_dur(float dur) {
 
 int main(void) {
     std::ifstream f(filename);
-    float last_event_time = 0;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     //std::cout << begin.time_since_epoch().count() / 1000 << std::endl;
     for (const event e : read_events(f)) {
         std::chrono::steady_clock::time_point event_time = begin + float_to_dur(e.start);
         //std::cout << event_time.time_since_epoch().count() / 1000 << std::endl;
         std::this_thread::sleep_until(event_time);
-        std::cout << "x " << e.x << " y " << e.y << " s " << e.start << " d " << e.duration << std::endl;
+        std::cout << "ON: x " << e.x << " y " << e.y << " s " << e.start << " d " << e.duration << std::endl;
+        if (e.duration) {
+            event_time = event_time + float_to_dur(e.duration);
+            std::this_thread::sleep_until(event_time);
+        }
+        std::cout << "OFF" << std::endl;
     }
     return 0;
 }
