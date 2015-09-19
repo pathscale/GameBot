dd534c99e216382a045b27211d95e72a62b6ccee
D.C <pasc.pcz@porcupinefactory.org>
2015-09-19 16:42:00 +0200

    cookie: added replay delay

--- CookieRun/reader.cpp
+++ CookieRun/reader.cpp
@@ -60,12 +60,21 @@ std::chrono::duration<int, std::milli> float_to_dur(float dur) {
 }
 
 int main(int argc, char **argv) {
-    if (argc < 3) {
+    if (argc < 4) {
         std::cout << "usage: provide evdev path for the touch screen on your device together with log file to play back" << std::endl;
-        std::cout << "./player /dev/input/event0 recording.log" << std::endl;
+        std::cout << "./player /dev/input/event0 recording.log 1.23" << std::endl;
+        std::cout << "./player EVDEV CSV_SOURCE DELAY_SEC" << std::endl;
         std::cout << "hint: adb shell getevent" << std::endl;
         return 1;
     }
+    
+    float delay_sec = 0;
+    std::stringstream delay_conv(argv[3]);
+    if (!(delay_conv >> delay_sec)) {
+        std::cerr << "Offset must be float " << argv[3] << std::endl;
+        return 1;
+    }
+    
     std::ifstream f(argv[2]);
     if (f.fail()) {
         std::cerr << "Couldn't open " << argv[2] << std::endl;
@@ -76,11 +85,10 @@ int main(int argc, char **argv) {
         std::cerr << "ADB no device" << std::endl;
         return 1;
     }
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    //std::cout << begin.time_since_epoch().count() / 1000 << std::endl;
+    
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now() + float_to_dur(delay_sec);
     for (const event e : read_events(f)) {
         std::chrono::steady_clock::time_point event_time = begin + float_to_dur(e.start);
-        //std::cout << event_time.time_since_epoch().count() / 1000 << std::endl;
         std::this_thread::sleep_until(event_time);
         int desc = adb.start_touch(e.x, e.y);
         std::cout << "ON: x " << e.x << " y " << e.y << " s " << e.start << " d " << e.duration << std::endl;
